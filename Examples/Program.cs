8345551c3f239d864cc94100543ea5ebed2d648d
Vasiliev Sergei <sergei.binp@gmail.com>
2023-04-24 22:28:20 +0500

    1st commit: задача 1

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MaxDigit (int randomNum){
+    int dec = randomNum / 10;
+    int ed = randomNum % 10;
+    if (dec > ed) return dec;
+    else return ed;
+}
+
+int num = new Random().Next(10, 99 + 1);
+int result = MaxDigit(num);
+Console.WriteLine($"{num} = {result}");
