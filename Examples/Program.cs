40c5a5d51ef5ca841cc3ace4da34fb0a22da41a2
Vasiliev Sergei <sergei.binp@gmail.com>
2023-04-24 22:53:27 +0500

    3rd commit: задача 3

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -9,14 +9,39 @@
 // int result = MaxDigit(num);
 // Console.WriteLine($"{num} = {result}");
 
-bool Kratn (int num1, int num2){
-    if (num1 % num2 == 0) return true;
-    else return false;
-} 
+
+
+
+
+// bool Kratn (int num1, int num2){
+//     if (num1 % num2 == 0) return true;
+//     else return false;
+// } 
+
+// Console.WriteLine("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"{num1}, {num2}, --> {Kratn(num1, num2)}, остаток равен {num1 % num2}");
+
+
+
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14  ->  нет
+// 46  ->  нет
+// 161 ->  да
+
 
 Console.WriteLine("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
+bool Kratn (int num){
+    if ((num % 7 == 0) && (num % 23 == 0)) return true;
+    else return false;
+}
+Console.WriteLine($"Введено число {num}, Кратно ли оно 7 и 23 одновременно - {Kratn(num)}");
 
-Console.WriteLine($"{num1}, {num2}, --> {Kratn(num1, num2)}, остаток равен {num1 % num2}");
+
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
