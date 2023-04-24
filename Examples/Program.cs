d50287e138d84ff3dd3af4f85e7a529c5ff3b569
Vasiliev Sergei <sergei.binp@gmail.com>
2023-04-24 23:02:56 +0500

    4th commit: задача 4

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -34,14 +34,26 @@
 // 161 ->  да
 
 
-Console.WriteLine("Введите первое число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-bool Kratn (int num){
-    if ((num % 7 == 0) && (num % 23 == 0)) return true;
-    else return false;
-}
-Console.WriteLine($"Введено число {num}, Кратно ли оно 7 и 23 одновременно - {Kratn(num)}");
+// Console.WriteLine("Введите первое число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// bool Kratn (int num){
+//     if ((num % 7 == 0) && (num % 23 == 0)) return true;
+//     else return false;
+// }
+// Console.WriteLine($"Введено число {num}, Кратно ли оно 7 и 23 одновременно - {Kratn(num)}");
 
 
 
 // Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool Kv (int num1, int num2){
+    if ((num1 == num2 * num2) || (num2 == num1*num1)) return true;
+    else return false;
+}
+if (Kv(num1, num2) == true) Console.WriteLine($"Одно число является квадратом другого");
+else Console.WriteLine($"Одно число НЕ является квадратом другого");
